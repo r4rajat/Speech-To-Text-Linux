060b55c2b008a43d1c50d04ac5d1e69d3a8f820a
Rajat Gupta <37516416+r4rajat@users.noreply.github.com>
2020-07-30 20:06:24 +0530

    Added Project Files

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Speech-To-Text-Linux-
 A Speech to Text Converter for Linux Environments using Python3
+
+### Step 1
+Install Package Dependencies for Linux
+
+```shell script
+$ sudo apt-get install -y python3-pyaudio flac portaudio19-dev python-all-dev python3-all-dev
+```
+
+### Step 2
+Install Python Dependencies
+
+###### Note : Creation of Virtual Environment Recommended
+```shell script
+(venv) $ pip3 install -r requirements.txt 
+```
+
+### Step 3
+Run the Program
+```shell script
+(venv) $ python3 speechtotext.py
+```
+
---
+++ speechtotext.py
@@ -0,0 +1,19 @@
+import speech_recognition as sr
+
+recognizer = sr.Recognizer()
+
+while True:
+    try:
+        with sr.Microphone() as source:
+            recognizer.adjust_for_ambient_noise(source, duration=0.2)
+            print("Please Speak Now..")
+            audio = recognizer.listen(source)
+            text_in_audio = recognizer.recognize_google(audio)
+            text_in_audio = text_in_audio.lower()
+            print("\n You Just Said : ", text_in_audio)
+
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
+
+    except sr.UnknownValueError:
+        print("unknown error occured")
